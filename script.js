29ed88173a50245c6aa2f79fe9d41e765daca790
Fabiana Helena <94461288+FabiiHelena@users.noreply.github.com>
2024-01-26 09:46:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
 
-const greetingElement = document.getElementById("greeting");
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
 
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
-const currentHour = new Date().getHours();
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
 
+    resultArtist.classList.remove('hidden');
+}
 
-const greetingMessage =
-  currentHour >= 5 && currentHour < 12
-    ? "Bom dia"
-    : currentHour >= 12 && currentHour < 18
-    ? "Boa tarde"
-    : "Boa noite";
-
-greetingElement.textContent = greetingMessage;
-
-
-const container = document.querySelector(".offer__list-item");
-
-const observer = new ResizeObserver(() => {  
-  const containerWidth = container.offsetWidth; 
-  const numColumns = Math.floor(containerWidth / 200); 
-
-  
-  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
-
-  console.log({ container });
-  console.log({ numColumns });
-});
-
-observer.observe(container);
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
